0094f06fc075a62ff9382ca4baaef6901279619a
adrian <adrian@okwave.global>
2020-10-05 09:38:23 +0800

    [frontend] - UI updates - add material-table

--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -1,44 +1,48 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import MaterialTable from 'material-table'
 
 export default function HomePage() {
+
+    const [data, setData] = useState([])
+    const [colums, setColums] = useState([
+        { title: 'Info', field: 'info' }
+    ])
+    useEffect(() => {
+        handlegetRequest()
+    }, []);
 
     const handlegetRequest = () => {
         axios.get('http://localhost:4000/quotation').then((res) => {
             if (res.data.length > 0) {
                 setData(res.data)
-                setShowDeletebtn(true)
+                console.log("handlegetRequest -> res.data", res.data)
             } else {
-                setData([
-                    {
-                        _id: 'null',
-                        info: 'No entries found'
-                    }
-                ])
-                setShowDeletebtn(false)
+                setData([])
             }
         })
     }
 
-    const handlepostRequest = () => {
+    const handlepostRequest = (newdata) => {
         let data = {
-            info: value,
+            info: newdata.info,
             valid: true
         }
 
-        if (value == '') {
+        if (newdata.info ==='') {
             alert('Empty')
         } else {
             axios.post('http://localhost:4000/quotation', data).then((res) => {
                 console.log("handlegetRequest -> res", res.data)
                 if (res.data) {
-                    setValue('')
+                    handlegetRequest()
                 }
             })
         }
     }
 
     const handledeleteRequest = (id) => {
+        console.log("handledeleteRequest -> id", id)
         let data = {
             id
         }
@@ -49,34 +53,50 @@ export default function HomePage() {
         })
     }
 
-    const handleChange = (e) => {
-        setValue(e.target.value)
-    }
-
-    const [data, setData] = useState([])
-    const [value, setValue] = useState('')
-    const [showDeletebtn, setShowDeletebtn] = useState(false)
-
     return (
         <div className="App">
             <div className="App">
                 <center>
                     <h1>Full Stack Developer Test</h1>
-                    <section className="container">
-                        <label>
-                            Quotation Info:
-                                <input type="text" value={value} onChange={(e) => handleChange(e)} />
-                        </label>
-                    </section>
-                    <button onClick={() => { handlegetRequest() }}>GET</button>
-                    <button onClick={() => { handlepostRequest() }}>POST</button>
                 </center>
-                {data.map(data => (
-                    <div key={data._id}>
-                        { showDeletebtn && <button onClick={() => { handledeleteRequest(data._id) }}>Delete</button>}
-                        {data.info}
-                    </div>)
-                )}
+
+                {/* Table */}
+                <MaterialTable
+                    title="Quotation"
+                    columns={colums}
+                    data={data}
+                    actions={[
+                        {
+                            icon: 'delete',
+                            tooltip: 'Delete',
+                            onClick: (event, rowData) => {
+                                handledeleteRequest(rowData._id)
+                            }
+                        }
+                    ]}
+                    editable={{
+                        onRowAdd: newData =>
+                            new Promise((resolve, reject) => {
+                                if (Object.keys(newData).length === 0 && newData.constructor === Object) {
+                                    alert('Empty')
+                                    resolve();
+                                } else {
+                                    setTimeout(() => {
+                                        handlepostRequest(newData)
+                                        resolve();
+                                    }, 500)
+                                }
+                            }),
+                    }}
+                    options={{
+                        search: false,
+                        actionsColumnIndex: -1,
+                        emptyRowsWhenPaging: false,
+                        showFirstLastPageButtons: false,
+                        paging: false,
+                        headerStyle: { fontSize: 13, fontWeight: 'bold', backgroundColor: '#F5F5F5' }
+                    }}
+                />
             </div>
         </div>
     );
